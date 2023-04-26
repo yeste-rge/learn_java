f67b7994ac7027c37839d1409238b5a066562a61
yeste-rge <Cheongziqian20@gmail.com>
2023-04-26 23:20:55 +0800

    ..

--- 彭彭/First.java
+++ 彭彭/First.java
@@ -1,17 +1,18 @@
 package 彭彭;
 
-public class Hello{
+public class First{
   public static void main(String[] args){
-    System.out.println("Hello Java"); 
+    System.out.println("Hello World"); 
     System.out.println("My First Java Program"); 
   }
 }
 
 
+
 //aws
 // package 彭彭;
 
-// public class Hello{
+// public class First{
 //     public static void main(String[] args){
 //     System.out.println("Hello World"); 
 //   }
