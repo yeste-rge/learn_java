2d658ab2708b3efb1dacaa5f9dacdbd1047c4c21
yeste-rge <Cheongziqian20@gmail.com>
2023-04-26 21:54:17 +0800

    。。。

--- 彭彭/Hello.java
+++ 彭彭/Hello.java
@@ -2,6 +2,17 @@ package 彭彭;
 
 public class Hello{
   public static void main(String[] args){
-    System.out.println("Hello Java");
+    System.out.println("hello Java"); 
+    System.out.println("My First Java Program"); 
   }
 }
+
+
+//aws
+// package 彭彭;
+
+// public class Hello{
+//     public static void main(String[] args){
+//     System.out.println("Hello World"); 
+//   }
+// }
