88942fcf0d9a29c7e3b2305f2ea081ea9dd33f3d
yeste-rge <Cheongziqian20@gmail.com>
2023-04-28 09:37:10 +0800

    ...

--- DataTypeVariable.java
+++ DataTypeVariable.java
@@ -1,5 +1,23 @@
 public class DataTypeVariable{
     public static void main(String[] args){
         // 在终端机印出各种资料形态的资料
+        // 整数,预设是 int 形态
+        System.out.println(3);
+        System.out.println(120);
+        // 长整数,数字的后面特别加上 L
+        System.out.println(2000000000000L);
+        // 浮点数,预设为 double 形态
+        System.out.println(3.14159268);
+        // 单精度浮点数,数字的后面特别加上 F
+        System.out.println(3.14159268F);
+        // 布林值
+        System.out.println(false);    
+        //字元
+        System.out.println('a');
+        // 字串:可以包含 0 到多个字元
+        System.out.println("哈喽你好");
+        //System.out.println("");
+
     }
+
 }
