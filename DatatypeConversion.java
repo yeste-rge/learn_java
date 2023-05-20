3e89d4806594ba8b655f79daf81ad965c5d2483c
yeste-rge <Cheongziqian20@gmail.com>
2023-05-20 14:02:08 +0800

    同do

---
+++ DatatypeConversion.java
@@ -0,0 +1,10 @@
+public class DatatypeConversion {
+    public static void main(String[] args){
+        // 数字型状态的转换
+        // 小范围转到大范围
+        // 大范围转到小范围 : 需要使用(资料形态)做强制转换
+        
+        // 字串
+        //
+    }
+}
