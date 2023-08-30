cdf9dfcb1994044d809e5060088949ff6c93b9b8
yeste-rge <Cheongziqian20@gmail.com>
2023-08-30 10:51:42 +0800

    Update DatatypeConversion.java

--- DatatypeConversion.java
+++ DatatypeConversion.java
@@ -1,5 +1,9 @@
 public class DatatypeConversion {
     public static void main(String[] args){
+        //在终端机印出各种资料形态的资料
+        //整数 ，预设是 int 形态
+        System.out.println(3);
+        System.out.println(120);
         // 数字型状态的转换
         // 小范围转到大范围
         // 大范围转到小范围 : 需要使用(资料形态)做强制转换
